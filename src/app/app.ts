a7293924829f69e8b52c614bdfaeb07319e632b7
SydCrow-bit <kevincm400@gmail.com>
2025-10-27 21:26:44 -0400

    feat(web): vista tabla + formulario insertar empleados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmpleadosComponent } from './components/empleados/empleados';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmpleadosComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
